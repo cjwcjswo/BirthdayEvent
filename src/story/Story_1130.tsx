c457eedf433c0230efdd9b6aa39dbabdaa8731e6
cjwoov <chlwlsdn37@gmail.com>
2022-05-05 09:52:42 +0900

    condition add

--- src/story/Story_1130.tsx
+++ src/story/Story_1130.tsx
@@ -6,29 +6,51 @@ import {
     Container,
 } from 'react-bootstrap'
 
+const CONDITION_TIME = new Date(2022, 4, 5, 14, 0);
 const Story_1130 = () => {
+    const isConditionTime = () => {
+        return new Date().getTime() > CONDITION_TIME.getTime();
+    }
+
+    const content = () => {
+        if (isConditionTime()) {
+            return (
+                    <Container fluid>
+                        <h1>4. 중식 코스요리집 난향</h1>
+                        <h1>(Code: 1130)</h1>
+                        점심은 맛있게 먹었니 김펭긴❤? <br />
+                        제대로된 중식 코스요리라고 하기도 뭐하지만<br />
+                        똥폴베는 중식 코스요리가 처음이였다고 해 🐣<br />
+                        우리의 다음 일정은 너도 예상하다 싶이<br />
+                        <span style={{ color: 'skyblue' }}><b>방탈출</b></span>이야! <br />
+                        어린이날이라 인기있는 방탈출은 예상대로 꽉 차 있었지만...<br />
+                        신촌에서 1위 테마인<br />
+                        <span style={{ color: 'skyblue' }}><b>오즈 익시드 드림</b></span>이란 테마를 하러 간단다 😛<br />
+                        <br />
+                        <div className="img-div">
+                            <img src={oz} />
+                        </div>
+                        <br />
+                        자 여기서 문제! 룸이스케이프 올리브점에서<br />
+                        똥폴베랑 예전에 방탈출을 한적이 있는데<br />
+                        그 테마의 이름은 무엇일까???<br />
+                        <span style={{ color: 'red' }}><b>테마 이름의 글자 갯수</b></span>가 정답이야!<br />
+                    </Container>
+            );
+        } else {
+            return (
+                <Container fluid>
+                    <h1>해당 문제는</h1>
+                    <h1>{CONDITION_TIME.toLocaleString()}</h1>
+                    <h1>에 열립니다.</h1>
+                </Container>
+            );
+        }
+
+    }
+
     return (
-        <Container fluid>
-            <h1>4. 중식 코스요리집 난향</h1>
-            <h1>(Code: 1130)</h1>
-            점심은 맛있게 먹었니 김펭긴❤? <br />
-            제대로된 중식 코스요리라고 하기도 뭐하지만<br />
-            똥폴베는 중식 코스요리가 처음이였다고 해 🐣<br />
-            우리의 다음 일정은 너도 예상하다 싶이<br />
-            <span style={{ color: 'skyblue' }}><b>방탈출</b></span>이야! <br />
-            어린이날이라 인기있는 방탈출은 예상대로 꽉 차 있었지만...<br />
-            신촌에서 1위 테마인<br />
-            <span style={{ color: 'skyblue' }}><b>오즈 익시드 드림</b></span>이란 테마를 하러 간단다 😛<br />
-            <br />
-            <div className="img-div">
-                <img src={oz} />
-            </div>
-            <br />
-            자 여기서 문제! 룸이스케이프 올리브점에서<br />
-            똥폴베랑 예전에 방탈출을 한적이 있는데<br />
-            그 테마의 이름은 무엇일까???<br />
-            <span style={{ color: 'red' }}><b>테마 이름의 글자 갯수</b></span>가 정답이야!<br />
-        </Container>
+        <>{content()}</>
     );
 }
 
--- src/story/Story_12.tsx
+++ src/story/Story_12.tsx
@@ -6,32 +6,56 @@ import {
     Container,
 } from 'react-bootstrap'
 
+const CONDITION_TIME = new Date(2022, 4, 5, 15, 0);
+
 const Story_12 = () => {
+    const isConditionTime = () => {
+        return new Date().getTime() > CONDITION_TIME.getTime();
+    }
+
+    const content = () => {
+        if (isConditionTime()) {
+            return (
+                <Container fluid>
+                    <h1>5. 우리는 방탈출 덕후</h1>
+                    <h1>(Code: 12)</h1>
+                    과연 방을 무사히 탈출했을까?? <br />
+                    그래! 예전에 똥폴베와 했던<br />
+                    대저택 레테의 잠겨버린 출구는 장치로만 이루어진 방!<br />
+                    이번 오즈 익시드 드림은 재밌었나 모르겠네 😋<br />
+                    어느새 하루의 반이 지나갔구나 <br />
+                    언제나 그렇듯 시간이 참 빠르게 흘러갔겠지? <br />
+                    정말 뜬금없지만 갑자기<br />
+                    둘이 여행갔던 날들이 생각나는구나<br />
+                    <br />
+
+                    <div className="img-div">
+                        <img src={oe} />
+                    </div>
+                    <br />
+                    이 날 기억하니?? 같이 오이도를 갔던 날이야<br />
+                    밤 바람도 제대로 쐬고, 폭죽도 보고, 점도 보고<br />
+                    <span style={{ color: 'red' }}><b>이 날이 몇년 몇월 몇일이였더라...??</b></span><br />
+                    <span style={{ color: 'red' }}>□□□□□□□□</span>
+                    <br /><br /><br />
+                </Container>
+            );
+        } else {
+            return (
+                <Container fluid>
+                    <h1>해당 문제는</h1>
+                    <h1>{CONDITION_TIME.toLocaleString()}</h1>
+                    <h1>에 열립니다.</h1>
+                </Container>
+            );
+        }
+    }
+
     return (
-        <Container fluid>
-            <h1>5. 우리는 방탈출 덕후</h1>
-            <h1>(Code: 12)</h1>
-            과연 방을 무사히 탈출했을까?? <br/>
-            그래! 예전에 똥폴베와 했던<br/>
-            대저택 레테의 잠겨버린 출구는 장치로만 이루어진 방!<br/>
-            이번 오즈 익시드 드림은 재밌었나 모르겠네 😋<br/>
-            어느새 하루의 반이 지나갔구나 <br/>
-            언제나 그렇듯 시간이 참 빠르게 흘러갔겠지? <br/>
-            정말 뜬금없지만 갑자기<br/>
-            둘이 여행갔던 날들이 생각나는구나<br/>
-            <br/>
-            
-            <div className="img-div">
-                <img src={oe}/>
-            </div>
-            <br/>
-            이 날 기억하니?? 같이 오이도를 갔던 날이야<br/>
-            밤 바람도 제대로 쐬고, 폭죽도 보고, 점도 보고<br/>
-            <span style={{color:'red'}}><b>이 날이 몇년 몇월 몇일이였더라...??</b></span><br/>
-            <span style={{color:'red'}}>□□□□□□□□</span>
-            <br/><br/><br/>
-        </Container>
-    );
+        <>
+            {content()}
+        </>
+    )
 }
 
 export { Story_12 };
